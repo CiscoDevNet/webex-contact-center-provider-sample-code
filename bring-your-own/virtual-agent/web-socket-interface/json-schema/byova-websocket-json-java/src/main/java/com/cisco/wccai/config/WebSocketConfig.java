ac8af9752a46ace6502cc26673f88aa4fc22bb22
Sambhav Khare <sambhav.khare.in@gmail.com>
2026-05-11 21:53:18 +0530

    add jwt validation

--- bring-your-own/virtual-agent/web-socket-interface/json-schema/byova-websocket-json-java/src/main/java/com/cisco/wccai/config/WebSocketConfig.java
+++ bring-your-own/virtual-agent/web-socket-interface/json-schema/byova-websocket-json-java/src/main/java/com/cisco/wccai/config/WebSocketConfig.java
@@ -1,14 +1,15 @@
 package com.cisco.wccai.config;
 
+import com.cisco.wccai.auth.AuthorizationHandshakeInterceptor;
 import com.cisco.wccai.handler.ListVirtualAgentWebSocketHandler;
 import com.cisco.wccai.handler.VirtualAgentWebSocketHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
@@ -16,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 public class WebSocketConfig implements WebSocketConfigurer {
     private final VirtualAgentWebSocketHandler virtualAgentWebSocketHandler;
     private final ListVirtualAgentWebSocketHandler listVirtualAgentWebSocketHandler;
+    private final AuthorizationHandshakeInterceptor authorizationHandshakeInterceptor;
 
     @Value("${spring.websocket.max-text-message-buffer-size:10485760}")
     private int maxTextMessageBufferSize;
@@ -27,17 +29,21 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private long maxSessionIdleTimeout;
 
     public WebSocketConfig(VirtualAgentWebSocketHandler virtualAgentWebSocketHandler,
-                           ListVirtualAgentWebSocketHandler listVirtualAgentWebSocketHandler) {
+                           ListVirtualAgentWebSocketHandler listVirtualAgentWebSocketHandler,
+                           AuthorizationHandshakeInterceptor authorizationHandshakeInterceptor) {
         this.virtualAgentWebSocketHandler = virtualAgentWebSocketHandler;
         this.listVirtualAgentWebSocketHandler = listVirtualAgentWebSocketHandler;
+        this.authorizationHandshakeInterceptor = authorizationHandshakeInterceptor;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(virtualAgentWebSocketHandler, "/v1/va")
+                .addInterceptors(authorizationHandshakeInterceptor)
                 .setAllowedOrigins("*");
 
         registry.addHandler(listVirtualAgentWebSocketHandler, "/v1/listVirtualAgents")
+                .addInterceptors(authorizationHandshakeInterceptor)
                 .setAllowedOrigins("*");
     }
 
