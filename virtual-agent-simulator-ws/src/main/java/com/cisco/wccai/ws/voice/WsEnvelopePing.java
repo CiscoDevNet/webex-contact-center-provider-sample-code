5205fc1d0bb1e0deaf8d8184925429c2ddccb6bc
Sambhav Khare <sambhav.khare.in@gmail.com>
2026-05-11 12:25:47 +0530

    refector

--- virtual-agent-simulator-ws/src/main/java/com/cisco/wccai/ws/voice/WsEnvelopePing.java
+++
@@ -1,9 +0,0 @@
-package com.cisco.wccai.ws.voice;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class WsEnvelopePing extends WsEnvelopeBase {
-}
